59fa0df4f068fa6f892295a35ef560d96d89c024
Kai Mast <kaimast@cs.wisc.edu>
2024-02-28 17:06:29 -0800

    Use strings as object ids

--- microbench-worker/src/main/java/org/dbos/apiary/microbench/functions/NectarBatchCreate.java
+++ microbench-worker/src/main/java/org/dbos/apiary/microbench/functions/NectarBatchCreate.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 
 public class NectarBatchCreate extends PostgresFunction {
     public static final String insertKey = "INSERT INTO ObjectStore (ObjectId, Key, Val) VALUES (?, ?, ?);";
-    public static int runFunction(PostgresContext ctxt, int[] objectIds, int numEntries, int entrySize) throws SQLException {
-    	for (int objectId: objectIds) {
+    public static int runFunction(PostgresContext ctxt, String[] objectIds, int numEntries, int entrySize) throws SQLException {
+    	for (String objectId: objectIds) {
     		String value = new String(new char[entrySize]).replace('\0', 'x');
     		for (int key = 0; key < numEntries; key++) {
     			ctxt.executeUpdate(insertKey, objectId, key, value);
