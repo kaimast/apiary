91c67575623c7da5d07c060cf85fff18b68a1ca4
Aditya Jain <adityajain.aj10@gmail.com>
2024-02-29 20:39:59 -0600

    PGR: [temp] temporary fix primary ip

--- scripts/initialize_postgres_docker_replicated.sh
+++ scripts/initialize_postgres_docker_replicated.sh
@@ -6,6 +6,6 @@ CURR_DIR=$(dirname $(realpath $0))
 # Start Postgres Docker image.
 docker pull postgres:14.5-bullseye
 
-SCRIPT_DIR=$CURR_DIR PRIMARY_PORT=5432 REPLICATION_PORT=5433 PRIMARY_USER="postgres" PRIMARY_PWD="dbos" REPLICATION_USER="replicator" REPLICATION_PWD="replicator_password" docker-compose -f $CURR_DIR/replicated-postgres-compose.yaml up --force-recreate -d postgres_primary postgres_replica
+POSTGRES_PRIMARY=10.10.1.2 SCRIPT_DIR=$CURR_DIR PRIMARY_PORT=5432 REPLICATION_PORT=5433 PRIMARY_USER="postgres" PRIMARY_PWD="dbos" REPLICATION_USER="replicator" REPLICATION_PWD="replicator_password" docker-compose -f $CURR_DIR/replicated-postgres-compose.yaml up --force-recreate -d postgres_primary postgres_replica
 
 sleep 5
