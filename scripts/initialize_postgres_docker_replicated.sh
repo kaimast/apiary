f886ec595f6ec29ce6e4e7531fa0df447e2d94ca
Aditya Jain <adityajain.aj10@gmail.com>
2024-02-29 19:45:26 -0600

    PGR: fixup paths

--- scripts/initialize_postgres_docker_replicated.sh
+++ scripts/initialize_postgres_docker_replicated.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
-SCRIPT_DIR=$(dirname $(realpath $0))
+CURR_DIR=$(dirname $(realpath $0))
 
 # Start Postgres Docker image.
 docker pull postgres:14.5-bullseye
 
-PRIMARY_PORT=5432 REPLICATION_PORT=5433 PRIMARY_USER="postgres" PRIMARY_PWD="dbos" REPLICATION_USER="replicator" REPLICATION_PWD="replicator_password" docker-compose -f replicated-postgres-compose.yaml up -d postgres_primary postgres_replica
+SCRIPT_DIR=$CURR_DIR PRIMARY_PORT=5432 REPLICATION_PORT=5433 PRIMARY_USER="postgres" PRIMARY_PWD="dbos" REPLICATION_USER="replicator" REPLICATION_PWD="replicator_password" docker-compose -f $CURR_DIR/replicated-postgres-compose.yaml up -d postgres_primary postgres_replica
 
 sleep 5
